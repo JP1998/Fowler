8e34e42131a6009fd8db680ea7744fe87930e9d0
Jonny <littlebigjonny06@gmail.com>
2019-04-25 11:50:36 +0200

    Add first test case

---
+++ src/com/gnilkreb/fowler/test/RentalTest.java
@@ -0,0 +1,18 @@
+package com.gnilkreb.fowler.test;
+
+import com.gnilkreb.fowler.Movie;
+import com.gnilkreb.fowler.Rental;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RentalTest {
+
+    @Test
+    public void testMovieStored() {
+        final Movie movie = new Movie("8 Mile", Movie.REGULAR);
+        final Rental rent = new Rental(movie, 12);
+
+        assertEquals(movie, rent.getMovie(), "The supplied movie has not been stored.");
+    }
+}
