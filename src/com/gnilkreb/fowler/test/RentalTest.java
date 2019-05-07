3831c2339528156acc054cbe771c2e887aab6e48
Jonny <littlebigjonny06@gmail.com>
2019-05-07 09:52:58 +0200

    Add tests for checking validity of data

--- src/com/gnilkreb/fowler/test/RentalTest.java
+++ src/com/gnilkreb/fowler/test/RentalTest.java
@@ -5,6 +5,7 @@ import com.gnilkreb.fowler.Rental;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RentalTest {
 
@@ -22,5 +23,26 @@ public class RentalTest {
         final Rental rent = new Rental(movie, 12);
 
         assertEquals(12, rent.getDaysRented(), "The days of rental have not been stored.");
+    }
+
+    @Test
+    public void testNoMovieRentedCheck() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> { new Rental(null, 1); },
+                "There is no check for a rented movie being null."
+        );
+    }
+
+    @Test
+    public void testInvalidDaysRentedCheck() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    final Movie rentedMovie = new Movie("asdf", Movie.REGULAR);
+                    new Rental(rentedMovie, -1);
+                },
+                "The days rented are not being checked for validity."
+        );
     }
 }
